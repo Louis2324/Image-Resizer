0fab2ecea2e5e957c7246e61a4c809ea02ec24f7
Louis2324 <miguellouis2023@gmail.com>
2025-07-14 04:47:38 +0200

    Starter working files

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app , BrowserWindow }= require("electron");
+const path = require("path");
+
+const isMac = process.platform == "darwin";
+const isDev = process.env.NODE_ENV !== "production"
+function createMainWindow () {
+    const mainWindow = new BrowserWindow({
+        title:"Image Resizer",
+        width:isDev?1000:500,
+        height:600,
+    });
+
+    //dev-tools
+    if(isDev) mainWindow.webContents.openDevTools();
+    mainWindow.loadFile(path.join(__dirname,"./renderer/index.html"));
+}
+
+app.whenReady().then(()=>{
+    createMainWindow();
+
+    app.on('activate', ()=>{
+        if(BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+})
+
+app.on('window-all-closed', ()=>{
+    if(isMac){
+        app.quit();
+    }
+})
