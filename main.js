dd23150b557d4ec7287dbde1ddec9a8e1da6be6e
Louis2324 <miguellouis2023@gmail.com>
2025-07-14 10:15:16 +0200

    Added menu customization and multiple windows

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { app , BrowserWindow }= require("electron");
+const { app , BrowserWindow , Menu }= require("electron");
 const path = require("path");
 
 const isMac = process.platform == "darwin";
 const isDev = process.env.NODE_ENV !== "production"
+
 function createMainWindow () {
     const mainWindow = new BrowserWindow({
         title:"Image Resizer",
@@ -14,6 +15,15 @@ function createMainWindow () {
     if(isDev) mainWindow.webContents.openDevTools();
     mainWindow.loadFile(path.join(__dirname,"./renderer/index.html"));
 }
+function createAboutWindow () {
+    const aboutWindow = new BrowserWindow({
+        title:"About Image Resizer",
+        width:isDev?600:300,
+        height:300,
+    });
+
+    aboutWindow.loadFile(path.join(__dirname,"./renderer/about.html"));
+}
 
 app.whenReady().then(()=>{
     createMainWindow();
@@ -23,7 +33,33 @@ app.whenReady().then(()=>{
             createMainWindow();
         }
     });
-})
+
+     const mainMenu = Menu.buildFromTemplate(menu);
+     Menu.setApplicationMenu(mainMenu);
+});
+
+const menu = [
+...(isMac?[
+    {
+        label:app.name,
+        submenu:
+        [
+            {
+                label: 'About',
+            },
+        ]
+    }
+  ]:[]),
+ {
+   role: "fileMenu"
+ },
+ ...(!isMac?[{
+    label: 'Help',
+    submenu: [{
+        label:"About",
+    }],
+ }]:[]),
+];
 
 app.on('window-all-closed', ()=>{
     if(isMac){
